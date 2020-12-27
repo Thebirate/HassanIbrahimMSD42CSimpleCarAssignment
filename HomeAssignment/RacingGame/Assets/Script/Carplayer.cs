0b6ec39e91a40e1214b250d9132277ce510303c9
Thebirate <60749357+Thebirate@users.noreply.github.com>
2020-12-27 12:10:05 +0100

    Added the audio files and finished the scrolling background

---
+++ HomeAssignment/RacingGame/Assets/Script/Carplayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carplayer : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+
+        transform.position = new Vector3(newXPos,transform.position.y);
+
+
+    }
+    private void SetUpMoveBoundaries()
+    {
+        //setup the boundaries of the movement according to the camera
+        Camera gameCamera = Camera.main; // fetching the main camera
+        float padding = 0.5f;
+
+
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+
+    }
+
+
+
+}
